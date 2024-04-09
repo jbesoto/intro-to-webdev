12a4c42038376cc43c057509c165f34fe2a93ae7
Juan Becerra <jdb9056@nyu.edu>
2024-04-09 10:25:39 -0400

    Commit Week 10 Homework

---
+++ notes/Week10/hw/script.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fetchStockInfo = async (symbol) => {
+    symbol = symbol.toUpperCase();
+
+    const apiKey = "M9VGU3FFM5GNSKIS";
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const {
+      "Global Quote": {
+        "02. open": open,
+        "03. high": high,
+        "04. low": low,
+        "05. price": price,
+        "06. volume": volume,
+        "09. change": change,
+      },
+    } = data;
+
+    return { symbol, open, high, low, price, volume, change };
+  };
+
+  const renderStockCard = (stock) => {
+    const ticker_field = document.getElementById("ticker-symbol");
+    const price_field = document.getElementById("stock-price");
+    const stock_specs = document.getElementById("stock-specs");
+    const change_field = document.getElementById("stock-change");
+
+    // Clear previous data
+    ticker_field.textContent = "";
+    price_field.textContent = "";
+    stock_specs.innerHTML = "";
+    change_field.textContent = "";
+
+    ticker_field.append(stock.symbol);
+    const stats = `
+        <tr>
+          <th>Open</th>
+          <td>${stock.open}</td>
+        </tr>
+        <tr>
+          <th>High</th>
+          <td>${stock.high}</td>
+        </tr>
+        <tr>
+          <th>Low</th>
+          <td>${stock.low}</td>
+        </tr>
+        <tr>
+          <th>Volume</th>
+          <td>${stock.volume}</td>
+        </tr>`;
+
+    stock_specs.insertAdjacentHTML("beforeend", stats);
+    price_field.append("$" + stock.price);
+
+    if (parseFloat(stock.change) < 0) {
+      change_field.innerHTML = `<span class="material-symbols-outlined large-icon red">south</span>`;
+      change_field.classList.add("red");
+      change_field.classList.remove("green");
+    } else {
+      change_field.innerHTML = `<span class="material-symbols-outlined large-icon green">north</span>`;
+      change_field.classList.add("green");
+      change_field.classList.remove("red");
+    }
+  };
+
+  document.getElementById("search-btn").addEventListener("click", () => {
+    const symbolInput = document.getElementById("symbol-in").value;
+    if (symbolInput) {
+      fetchStockInfo(symbolInput)
+        .then((stock_data) => {
+          renderStockCard(stock_data);
+        })
+        .catch((err) => {
+          console.error("Error: ", err);
+        });
+    } else {
+      alert("Please enter a stock symbol.");
+    }
+  });
+});
